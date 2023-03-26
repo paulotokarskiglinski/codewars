800c95df53809a1fdd7fc461fe8bfd24c22657c9
Paulo Henrique Tokarski Glinski <paulotokarski@gmail.com>
2023-03-26 18:39:45 -0300

    Solved

---
+++ persistentBugger.ts
@@ -0,0 +1,22 @@
+/*
+* Persistent Bugger
+* https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/typescript
+*
+* Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+* For example (Input --> Output):
+* 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+* 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+* 4 --> 0 (because 4 is already a one-digit number)
+*/
+
+export const persistence = (num: number, total: number = 0): number => {
+  if (num < 10)
+    return total;
+  
+  const res = num.toString().split('').reduce((sum: number, item: string) => sum *= parseInt(item) , 1);
+
+  return res < 10 ? ++total : persistence(res, ++total);
+}
+
+console.log('#1', persistence(39), 'should be `3`')
+console.log('#3', persistence(999), 'should be `4`')
