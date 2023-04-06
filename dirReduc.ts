4ae3967666efbbfabdc65040db88c52e44f9c62b
Paulo Henrique Tokarski Glinski <paulotokarskiglinski@gmail.com>
2023-04-06 15:52:37 -0300

    Solved

--- dirReduc.ts
+++ dirReduc.ts
@@ -1,3 +1,12 @@
+/*
+* Directions Reduction
+* https://www.codewars.com/kata/550f22f4d758534c1100025a
+*
+* Once upon a time, on a way through the old wild mountainous west...
+* ... a man was given directions to go from one point to another. The directions were "NORTH", "SOUTH", "WEST", "EAST". Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too.
+* Going to one direction and coming back the opposite direction right away is a needless effort. Since this is the wild west, with dreadful weather and not much water, it's important to save yourself some energy, otherwise you might die of thirst!
+*/
+
 export const dirReduc = (arr: string[]): string[] => {
   const mapDirections: any = { "NORTH": "SOUTH", "SOUTH": "NORTH", "WEST": "EAST", "EAST": "WEST" };
 
@@ -10,5 +19,3 @@ export const dirReduc = (arr: string[]): string[] => {
 
   return arr;
 }
-
-console.log(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]), 'should be [WEST]')
