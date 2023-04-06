5343c0f04ae56d8f5c9b8e2834ba1d172cd64b76
Paulo Henrique Tokarski Glinski <paulotokarskiglinski@gmail.com>
2023-04-06 15:56:02 -0300

    Solved

--- dnaStrand.ts
+++ dnaStrand.ts
@@ -1,7 +1,17 @@
+/*
+* Complementary DNA
+* https://www.codewars.com/kata/554e4a2f232cdd87d9000038
+*
+* Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions"
+* for the development and functioning of living organisms.
+*
+* Example: (input --> output)
+* "ATTGC" --> "TAACG"
+* "GTAT" --> "CATA"
+*/
+
 export class Kata {
   static dnaStrand(dna: string) {
     return dna.split('').map(x => ({ 'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G' })[x]).join('');
   }
 }
-
-console.log('#1', Kata.dnaStrand('ATTGC'), 'should be `TAACG`')
