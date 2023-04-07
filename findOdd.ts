a3487f01dee15ef74c504384affd0982cf5a654f
Paulo Henrique Tokarski Glinski <paulotokarskiglinski@gmail.com>
2023-04-06 21:32:11 -0300

    Solved

--- findOdd.ts
+++ findOdd.ts
@@ -1,18 +1,24 @@
-// export const findOdd = (xs: number[]): number => {
-//   const map: any = {};
-//   let odd: number = 0;
-
-//   for (let i = 0; i < xs.length; i++)
-//     map[xs[i]] = map[xs[i]] ? map[xs[i]] += 1 : 1;
-
-//   for (let k in map)
-//     if (map[k] % 2 !== 0)
-//       odd = parseInt(k);
-
-//   return odd;
-// };
+/*
+* Find the odd int
+* https://www.codewars.com/kata/54da5a58ea159efa38000836
+*
+* Given an array of integers, find the one that appears an odd number of times.
+* There will always be only one integer that appears an odd number of times.
+*/
 
 export const findOdd = (xs: number[]): number => xs.find((x, i, a) => a.filter(y => y === x).length % 2 !== 0) || 0;
 
-console.log('#1', findOdd([1,1,1,1,1,1,10,1,1,1,1]), 'should be `10`');
-console.log('#2', findOdd([1, 1, 2]), 'should be `2`')
+// Solution 2
+// export const findOdd = (xs: number[]): number => {
+//   const map: any = {};
+//   let odd: number = 0;
+//
+//   for (let i = 0; i < xs.length; i++)
+//     map[xs[i]] = map[xs[i]] ? map[xs[i]] += 1 : 1;
+//
+//   for (let k in map)
+//     if (map[k] % 2 !== 0)
+//       odd = parseInt(k);
+//
+//   return odd;
+// };
