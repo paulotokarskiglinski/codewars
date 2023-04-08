1ad7ec679883161cb34e96b2715f882d3b1f091e
Paulo Henrique Tokarski Glinski <paulotokarskiglinski@gmail.com>
2023-04-08 16:58:40 -0300

    Solved

--- getMiddleCharacter.ts
+++ getMiddleCharacter.ts
@@ -1,8 +1,14 @@
+/*
+* Get the Middle Character
+* https://www.codewars.com/kata/56747fd5cb988479af000028
+*
+* You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+* #Examples:
+* Kata.getMiddle("test") should return "es"
+* Kata.getMiddle("testing") should return "t"
+*/
+
 export function getMiddle(s: string): string {
   const middle = (s.length % 2 === 0) ? (s.length / 2) - 1 : (s.length - 1) / 2;
   return s.slice(middle, middle + (s.length % 2 === 0 ? 2 : 1));
 }
-
-console.log('#1', getMiddle('test'), 'should return `es`');
-console.log('#2', getMiddle('testing'), 'should return `t`');
-console.log('#3', getMiddle('A'), 'should return `A`');
