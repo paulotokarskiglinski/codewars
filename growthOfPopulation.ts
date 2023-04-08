63b16aa4c9dc3afb6145dbb0d47ea9884f8f1bd4
Paulo Henrique Tokarski Glinski <paulotokarskiglinski@gmail.com>
2023-04-08 17:01:40 -0300

    Solved

--- growthOfPopulation.ts
+++ growthOfPopulation.ts
@@ -1,3 +1,12 @@
+/*
+* Growth of a Population
+* https://www.codewars.com/kata/563b662a59afc2b5120000c6
+*
+* In a small town the population is p0 = 1000 at the beginning of a year. 
+* The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. 
+* How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+*/
+
 export const nbYear = (p0: number, percent: number, aug: number, p: number): number => {
   let years: number = 0;
   let percn: number = percent / 100;
@@ -10,7 +19,3 @@ export const nbYear = (p0: number, percent: number, aug: number, p: number): num
 
   return years;
 }
-
-console.log('#1', nbYear(1500000, 0, 10000, 2000000), 'should be 50')
-console.log('#2', nbYear(1000, 2, 50, 1214), 'should be 4')
-//  console.log(p0, percent, aug, p)
