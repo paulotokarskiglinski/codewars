3a56efdbaceebcd187f3763273b5164f65dca008
Paulo Henrique Tokarski Glinski <paulotokarskiglinski@gmail.com>
2023-04-09 16:15:02 -0300

    Solved

--- hightAndLow.ts
+++ hightAndLow.ts
@@ -1,8 +1,17 @@
+/*
+* Highest and Lowest
+* https://www.codewars.com/kata/554b4ac871d6813a03000035
+*
+* In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+* Examples
+* highAndLow("1 2 3 4 5");  // return "5 1"
+* highAndLow("1 2 -3 4 5"); // return "5 -3"
+* highAndLow("1 9 3 4 -5"); // return "9 -5"
+*/
+
 export class Kata {
   static highAndLow(numbers: string): string {
     let arr = numbers.split(' ').map(x => parseInt(x));
     return Math.max(...arr) + ' ' + Math.min(...arr);
   }
 }
-
-console.log('#1', Kata.highAndLow('1 2 3'), 'should return 3 1');
