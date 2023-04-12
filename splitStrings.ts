cc165ea3e2fc787778f1f2f208ef7a6763e2fc92
Paulo Henrique Tokarski Glinski <paulotokarskiglinski@gmail.com>
2023-04-12 15:49:26 -0300

    Solved

--- splitStrings.ts
+++ splitStrings.ts
@@ -1,3 +1,13 @@
+/*
+* Split Strings
+* https://www.codewars.com/kata/515de9ae9dcfc28eb6000001
+*
+* Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+* Examples:
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+*/
+
 export const solution = (str: string): string[] => {
   let result: string[] = [];
   const arr: string[] = str.split('');
@@ -7,6 +17,3 @@ export const solution = (str: string): string[] => {
 
   return result;
 }
-
-console.log(solution('abc'), ['ab', 'c_'])
-console.log(solution('abcdef'), ['ab', 'c_'])
