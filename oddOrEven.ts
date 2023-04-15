fc7344546a2267dcde818d57874b506a39e708c2
Paulo Henrique Tokarski Glinski <paulotokarskiglinski@gmail.com>
2023-04-15 15:00:26 -0300

    Solved

--- oddOrEven.ts
+++ oddOrEven.ts
@@ -1,4 +1,10 @@
-export const oddOrEven = (array: number[]) => (array.reduce((sum, value) => sum + value, 0)) % 2 === 0 ? 'even' : 'odd';
+/*
+* Odd or Even?
+* https://www.codewars.com/kata/5949481f86420f59480000e7
+*
+* Given a list of integers, determine whether the sum of its elements is odd or even.
+* Give your answer as a string matching "odd" or "even".
+* If the input array is empty consider it as: [0] (array with a zero).
+*/
 
-console.log(oddOrEven([0, 1, 2]))
-console.log(oddOrEven([0]))
+export const oddOrEven = (array: number[]) => (array.reduce((sum, value) => sum + value, 0)) % 2 === 0 ? 'even' : 'odd';
