97ccd937a116c1633a3fb585e9f676ca9b744c7c
Paulo Henrique Tokarski Glinski <paulotokarskiglinski@gmail.com>
2023-05-10 13:57:21 -0300

    Solved

---
+++ doubleInteger.ts
@@ -0,0 +1,8 @@
+/*
+* You Can't Code Under Pressure #1
+* https://www.codewars.com/kata/53ee5429ba190077850011d4
+*
+* Code as fast as you can! You need to double the integer and return it.
+*/
+
+export const doubleInteger = (i: number): number => i * 2;
