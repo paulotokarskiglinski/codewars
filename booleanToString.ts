dc8775f18bbfc64f584895fc6bd46c43c086e52c
Paulo Henrique Tokarski Glinski <paulotokarskiglinski@gmail.com>
2023-05-10 13:57:53 -0300

    Solved

---
+++ booleanToString.ts
@@ -0,0 +1,8 @@
+/*
+* Convert a Boolean to a String
+* https://www.codewars.com/kata/551b4501ac0447318f0009cd
+*
+* Implement a function which convert the given boolean value into its string representation.
+*/
+
+export const booleanToString = (b: boolean): string => b ? 'true' : 'false';
