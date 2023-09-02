67878250a2d5c612cbe5f570cf9a92640eaada1f
Paulo Henrique Tokarski Glinski <paulotokarskiglinski@gmail.com>
2023-09-02 19:42:31 +0100

    Solved

---
+++ deleteNth.ts
@@ -0,0 +1,22 @@
+/**
+ * Delete occurrences of an element if it occurs more than n times
+ * https://www.codewars.com/kata/554ca54ffa7d91b236000023
+ * 
+ * Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+ * For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+ * With list [20,37,20,21] and number 1, the result would be [20,37,21].
+ */
+
+export const deleteNth = (arr: number[], n: number): number[] => {
+  const hash = {};
+  const result: number[] = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    hash[arr[i]] ? hash[arr[i]]++ : hash[arr[i]] = 1;
+    
+    if (hash[arr[i]] <= n)
+      result.push(arr[i]);
+  }
+  
+  return result;
+}
