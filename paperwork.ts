707a32d42a66b4411aa34da3c2c3c9b5c12679a3
Paulo Henrique Tokarski Glinski <paulotokarskiglinski@gmail.com>
2024-03-23 15:38:07 +0000

    Solved

---
+++ paperwork.ts
@@ -0,0 +1,8 @@
+/*
+* Beginner Series #1 School Paperwork
+* https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
+*/
+
+export const paperwork = (n: number, m: number): number => {
+  return n > 0 && m > 0 ? n * m : 0;
+}
