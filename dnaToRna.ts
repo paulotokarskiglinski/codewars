4a6115b052f4ceb2b2738fd812fe9c0c5396b3c8
Paulo Henrique Tokarski Glinski <paulotokarskiglinski@gmail.com>
2024-05-08 17:11:43 +0100

    Solved

---
+++ dnaToRna.ts
@@ -0,0 +1,6 @@
+/*
+ * DNA to RNA Conversion
+ * https://www.codewars.com/kata/5556282156230d0e5e000089
+ */
+
+export const DNAtoRNA = (dna: string): string => dna.replaceAll('T', 'U');
---
+++ minMaxFunc.ts
@@ -0,0 +1,8 @@
+/*
+ * Find Maximum and Minimum Values of a List
+ * https://www.codewars.com/kata/577a98a6ae28071780000989
+ */
+
+export const min = (list: number[]): number => Math.min(...list);
+
+export const max = (list: number[]): number => Math.max(...list);
