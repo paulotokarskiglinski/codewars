167e55344765987a102a9e7c917326686a071a73
Paulo Henrique Tokarski Glinski <paulotokarskiglinski@gmail.com>
2025-03-03 16:57:48 +0000

    Solved

--- productFib.ts
+++ productFib.ts
@@ -3,19 +3,34 @@
  * https://www.codewars.com/kata/5541f58a944b85ce6d00006a
  */
 
-export const productFib = (prod: number): [number, number, boolean] => {
-  let result = 0;
-  let f = [0, 1, 1];
+// Solution 2
+export const productFib = (prod:number): [number,number,boolean] => {
+  let prevValue = 0;
+  let nextValue = 1;
 
-  while (result <= prod) {
-    f.push(f[f.length - 1] + f[f.length - 2]);
-
-    result = f[f.length - 1] * f[f.length - 2];
-    
-    if (result >= prod) {
-      return [f[f.length - 2], f[f.length - 1], result === prod];
-    }
+  while (prevValue * nextValue < prod) {
+    const aux = prevValue;
+    prevValue = nextValue;
+    nextValue = aux + nextValue;
   }
 
-  return [-1, -1, false];
+  return [prevValue, nextValue, prevValue * nextValue === prod];
 }
+
+// Solution 1
+// export const productFib = (prod: number): [number, number, boolean] => {
+//   let result = 0;
+//   let f = [0, 1, 1];
+
+//   while (result <= prod) {
+//     f.push(f[f.length - 1] + f[f.length - 2]);
+
+//     result = f[f.length - 1] * f[f.length - 2];
+    
+//     if (result >= prod) {
+//       return [f[f.length - 2], f[f.length - 1], result === prod];
+//     }
+//   }
+
+//   return [-1, -1, false];
+// }
